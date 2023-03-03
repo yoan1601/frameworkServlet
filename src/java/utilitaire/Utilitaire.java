0bd081c683d6ef6c302d68ba4f0c9a48263eb0ff
Yoan <yoan.rab@gmail.com>
2023-03-03 14:28:05 +0300

    1stPush

---
+++ src/java/utilitaire/Utilitaire.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utilitaire;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author yoan
+ */
+public class Utilitaire {
+    public static String getURLPattern(HttpServletRequest request) throws Exception {
+        String rep = request.getPathInfo();
+        if(rep == null){
+            rep = "/";
+        }
+        return rep;
+    }
+}
