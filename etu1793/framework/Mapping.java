90926edfef38b7103c1b9e5707d318294d1528a0
Yoan <yoan.rab@gmail.com>
2023-03-14 15:52:18 +0300

    begin sprint3

--- etu1793/framework/Mapping.java
+++ etu1793/framework/Mapping.java
@@ -1,14 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package etu1793.framework;
 
-/**
- *
- * @author yoan
- */
+package etu1793.framework;
+import etu1793.framework.annotationDao.UrlAnnotation;
+
+
 public class Mapping {
     String className;
     String method;
+
+	public String getClassName() {
+		return this.className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getMethod() {
+		return this.method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+    @UrlAnnotation(urlPattern = "tenaManao")
+    public void myMethode() {
+        System.out.println("tena manao");
+    }
 }
