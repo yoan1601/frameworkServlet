ac090c1c741c0548bc7607ae59e9f3b1af369207
Yoan <yoan.rab@gmail.com>
2023-06-14 21:41:56 +0300

    1st comm

--- framework_pure/src/Session.java
+++ framework_pure/src/Session.java
@@ -4,6 +4,5 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Scope {
-    String type();
+public @interface Session {
 }
